a5ad548297438242c10f2782b85d7c3a7da6d9b2
Gabriel <gabriel.magnus.wright@gmail.com>
2025-08-11 15:38:30 +0100

    Creating structure

---
+++ script.js
@@ -0,0 +1,5 @@
+// DOM Elements
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz-screen");
+const resultScreen = document.getElementById("result-screen");
+const startButton = document.getElementById("start-btn");
